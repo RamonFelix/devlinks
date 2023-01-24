f6580eddba0e6cf02e127771bc96693fd8cc523e
Ramon Felix <ramonfsoliveira@gmail.com>
2023-01-24 08:16:13 -0300

    clean code

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 function toggleMode(){
-  const html = document.documentElement
-  /*if (html.classList.contains('light')) {
-   html.classList.remove('light')
-  }
-  else {
-    html.classList.add('light')
-  }
-  */
+ const html = document.documentElement
  html.classList.toggle('light')
 
  const img = document.querySelector("#profile img")
@@ -17,5 +10,14 @@ function toggleMode(){
   img.setAttribute("src", "imagens/foto.png")
  }
 
+ 
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./imagens/image_light.png")
+  } else {
+    img.setAttribute("src", "imagens/foto.png")
+  }
   
 }
